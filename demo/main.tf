13ab5e0d370c03e5dda894fc492da6a4d02dd134
maheshwaranandh <maheshwaranandh@gmail.com>
2024-01-30 21:49:59 +0530

    updated the code for demo of the module

--- demo/main.tf
+++ demo/main.tf
@@ -1,3 +1,9 @@
 provider "aws" {
   region = var.region
 }
+module "s3_bucket" {
+  source = "https://github.com/maheshwaranandh/terraform_s3_module/tree/master"
+  bucket = var.name
+  acl = var.acl
+  versioning = var.versioning
+}
--- demo/variables.tf
+++ demo/variables.tf
@@ -2,3 +2,15 @@ variable "region" {
   description = "The name of the aws region to be used"
   default = "us-east-1"
 }
+variable "acl" {
+  description = "Access control list of the bucket"
+  default = "private"
+}
+variable "name" {
+  description = "globally unique name for the bucket"
+  default = "maheshwar1811terramodulle"
+}
+variable "versioning" {
+  description = "wheather versioning is enabled or not"
+  default = true
+}
