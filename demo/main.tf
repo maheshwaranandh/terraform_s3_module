5b23f730d9a42ef4dc38a2d6107399b170dbebd2
maheshwaranandh <maheshwaranandh@gmail.com>
2024-01-30 22:11:33 +0530

    fixed the acl permissions

--- demo/main.tf
+++ demo/main.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = var.region
 }
 module "s3_bucket" {
-  source = "https://github.com/maheshwaranandh/terraform_s3_module/tree/master"
-  bucket = var.name
-  acl = var.acl
+  source = "github.com/maheshwaranandh/terraform_s3_module"
+  bucket_name = var.name
+  bucket_acl = var.acl
   versioning = var.versioning
 }
--- main.tf
+++ main.tf
@@ -1,13 +1,30 @@
 resource "aws_s3_bucket" "example_bucket" {
   bucket = var.bucket_name
 }
-resource "aws_s3_bucket_acl" "example_bicket_acl" {
-  bucket = aws_s3_bucket.example_bucket.id
-  acl = var.bucket_acl
-}
 resource "aws_s3_bucket_versioning" "example_bucket_versioning" {
   bucket = aws_s3_bucket.example_bucket.id
   versioning_configuration {
     status = var.versioning?"Enabled":"Disabled"
   }
 }
+resource "aws_s3_bucket_ownership_controls" "example_controls" {
+  bucket = aws_s3_bucket.example_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+resource "aws_s3_bucket_public_access_block" "example_blocks" {
+  count = var.bucket_acl == "public-read" || var.bucket_acl == "public-read-write" ? 1:0
+  bucket = aws_s3_bucket.example_bucket.id
+  block_public_acls = false
+  block_public_policy = false
+  ignore_public_acls = false
+  restrict_public_buckets = false
+
+}
+resource "aws_s3_bucket_acl" "example_bucket_acl" {
+  depends_on = [ aws_s3_bucket_ownership_controls.example_controls,aws_s3_bucket_public_access_block.example_blocks ]
+  bucket = aws_s3_bucket.example_bucket.id
+  acl = var.bucket_acl
+}
+
